5e783d86c727c4cda456094b7d229d87c0213bc5
lgcarlinf <lgcarlinf@unac.edu.pe>
2021-05-23 00:32:27 -0500

    new commit

---
+++ main.js
@@ -0,0 +1,22 @@
+AOS.init();
+
+
+const menuResponsive= document.querySelector(".menu-responsive");
+const btnMenu = document.querySelector(".btn_menu");
+const menuHamburguesa = document.querySelector("#hamburguesa");
+
+const toggleMenu = () =>{
+    menuResponsive.classList.toggle("traslate");
+    menuResponsive.style.transition = "transform .5s ease-in-out";
+}
+
+btnMenu.addEventListener('click', function(e){
+    toggleMenu();
+    
+})
+
+menuResponsive.addEventListener('click', function(e){
+    toggleMenu();
+   
+})
+
